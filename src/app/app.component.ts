6f2b62cd3aecccfa8a068002564b0767e654fd97
Eslam Amgad <163426064+EslamAmgad1@users.noreply.github.com>
2024-04-15 10:43:05 +0200

    Adding nav-bar commponent , product model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,NavBarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'E-commerceApp';
+  products : Product[] = [] ;
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>("https://localhost:7149/api/products").subscribe(
+      (response) => this.products=response.data
+    )
+  }
 }
