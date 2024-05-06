46608cf61013dba2043bbdcbc3530f422876430f
Eslam Amgad <163426064+EslamAmgad1@users.noreply.github.com>
2024-05-06 06:34:49 +0300

    Adding payment functionality with stripe

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -5,10 +5,13 @@ import { BusyService } from '../services/busy.service';
 
 export const loadingInterceptor: HttpInterceptorFn = (request, next) => {
   const busyService = inject(BusyService);
-  if (request.url.includes('emailExists')) {
+  if (
+    request.url.includes('emailExists') ||
+    request.method === 'POST' && request.url.includes('orders') ||
+    request.method === 'DELETE'
+  ) {
     return next(request);
   }
-
   busyService.busy();
   return next(request).pipe(
     delay(500),
